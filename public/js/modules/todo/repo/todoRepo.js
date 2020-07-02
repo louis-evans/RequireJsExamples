e874d68c9fdd94052d13d334d9d00d848c0b37ff
levans <levans@LT5592>
2020-07-02 16:22:31 +0100

    moved ui logic out of repo

--- public/js/modules/todo/repo/todoRepo.js
+++ public/js/modules/todo/repo/todoRepo.js
@@ -2,40 +2,21 @@ define(['../../utils/logger'], function(logger) {
 
     const todoList = document.getElementById('TodoList');
 
-    const addNewTask = (task) => {
-
+    const addNewTask = (task) => 
+    {
         return new Promise((resolve, reject) => 
         {
-            try
+            setTimeout(() => 
             {
-                const newId = createNewId();
-
-                const newItem = document.createElement('li');
-                newItem.classList.add('list-group-item');
-                newItem.dataset['id'] = newId;
-                newItem.innerHTML = task;
-
-                const subText = document.createElement('span');
-                subText.innerHTML = '<br/>Added: ' + moment().format('DD/MM/YYYY hh:mm:ss a');
-                subText.classList.add('text-muted');
-
-                const btnContainer = document.createElement('div');
-                btnContainer.classList.add('btn-container');
-                btnContainer.classList.add('pull-right');
-
-                btnContainer.appendChild(createCompleteButton(newId));
-                btnContainer.appendChild(createDeleteButton(newId));
-
-                newItem.appendChild(btnContainer);
-                newItem.appendChild(subText);
-                todoList.appendChild(newItem);
-                
-                resolve(newId);
-            }
-            catch(e)
-            {
-                reject(e);
-            }
+                try
+                {               
+                    resolve(createNewId());
+                }
+                catch(e)
+                {
+                    reject(e);
+                }
+            }, Math.floor(Math.random() * 2000));     
         });
     };
 
@@ -43,16 +24,18 @@ define(['../../utils/logger'], function(logger) {
     {
         return new Promise((resolve, reject) => 
         {
-            const task = todoList.querySelector("li[data-id='" + id + "']");
-
-            if(task === null)
+            setTimeout(() => 
             {
-                reject();
-                return;
-            }
-
-            task.remove();
-            resolve();
+                try
+                {               
+                    resolve();
+                }
+                catch(e)
+                {
+                    reject(e);
+                }
+            }, Math.floor(Math.random() * 1000));
+            
         });
     };
 
@@ -60,44 +43,8 @@ define(['../../utils/logger'], function(logger) {
     {
         return new Promise((resolve, reject) => 
         {
-            const task = todoList.querySelector("li[data-id='" + id + "']");
-
-            if(task === null)
-            {
-                reject();
-                return;
-            }
-
-            task.style.backgroundColor = '#dff0d8';
-            task.style.opacity = 0.5;
-
-            task.querySelector('.btn-container').remove();
             resolve();
         });
-    };
-
-    const createCompleteButton = (id) => 
-    {
-        const completeBtn = document.createElement('i');
-        completeBtn.dataset['for'] = id;
-        completeBtn.name = 'DoneTask';
-        completeBtn.classList.add('fa');
-        completeBtn.classList.add('fa-check');
-        completeBtn.classList.add('fa-2x');
-        completeBtn.classList.add('text-success');
-        return completeBtn;
-    };
-
-    const createDeleteButton = (id) => 
-    {
-        const deleteBtn = document.createElement('i');
-        deleteBtn.dataset['for'] = id;
-        deleteBtn.name = 'DeleteTask';
-        deleteBtn.classList.add('fa');
-        deleteBtn.classList.add('fa-times');
-        deleteBtn.classList.add('fa-2x');
-        deleteBtn.classList.add('text-danger');
-        return deleteBtn;
     };
 
     const createNewId = () => 
--- public/js/modules/todo/todo.js
+++ public/js/modules/todo/todo.js
@@ -4,26 +4,41 @@ requirejs.config({
 
 requirejs(['../utils/logger', 'repo/todoRepo', '../utils/dialog'], function(logger, repo, dialog) 
 {
-    logger.debug('Loaded todo module');
-
     const input = document.getElementById('EnterTask');
+    const btnAdd = document.getElementById('BtnAdd');
+    const todoList = document.getElementById('TodoList');
 
     const OnAddBtnClicked = () => 
     {
         const newTask = input.value;
-        if (!newTask) {
+
+        if (!newTask) 
+        {
             dialog.warn("Please enter a value!");
             return;
         }
+
+        input.disabled = true;
+        btnAdd.disabled = true;
+
         repo.addNewTask(newTask)
-            .then(id => {
-                logger.debug('Added new task: ' + id);
+            .then(newId => 
+            {
+                addTaskListItem(newTask, newId);
+
+                logger.debug('Added new task: ' + newId);
                 dialog.success('Task Added!');
-                input.value = '';
+                input.value = '';                
             })
-            .catch(e => {
+            .catch(e => 
+            {
                 logger.error('Error creating task!', e);
                 dialog.error('Could not create task!');
+            })
+            .finally(() => 
+            {
+                input.disabled = false;
+                btnAdd.disabled = false;
             });
     };
 
@@ -38,22 +53,82 @@ requirejs(['../utils/logger', 'repo/todoRepo', '../utils/dialog'], function(logg
         if(clickedBtn.name === 'DoneTask')
         {
             repo.markTaskDone(taskId)
-                .then(() => {
+                .then(() => 
+                {
+                    const task = findTaskById(taskId);
+
+                    task.style.backgroundColor = '#dff0d8';
+                    task.style.opacity = 0.5;
+
+                    task.querySelector('.btn-container').remove();
+
                     dialog.success('Task Completed!');
                 });
         }
-        else  if(clickedBtn.name === 'DeleteTask')
+        else if(clickedBtn.name === 'DeleteTask')
         {
             repo.deleteTask(taskId)
-                .then(() => {
+                .then(() => 
+                {
+                    const task = findTaskById(taskId);;
+                    task.remove();
+
                     dialog.success('Task Removed');
                 });
         }
-    
     };
 
-    document.getElementById('BtnAdd').addEventListener('click', OnAddBtnClicked);
+    const addTaskListItem = (task, id) => 
+    {
+        const newItem = document.createElement('li');
+        newItem.classList.add('list-group-item');
+        newItem.dataset['id'] = id;
+        newItem.innerHTML = task;
+
+        const subText = document.createElement('span');
+        subText.innerHTML = '<br/>Added: ' + moment().format('DD/MM/YYYY hh:mm:ss a');
+        subText.classList.add('text-muted');
+
+        const btnContainer = document.createElement('div');
+        btnContainer.classList.add('btn-container');
+        btnContainer.classList.add('pull-right');
+
+        btnContainer.appendChild(createCompleteButton(id));
+        btnContainer.appendChild(createDeleteButton(id));
+
+        newItem.appendChild(btnContainer);
+        newItem.appendChild(subText);
+        todoList.appendChild(newItem);
+    };
+
+    const createCompleteButton = (id) => 
+    {
+        const completeBtn = document.createElement('i');
+        completeBtn.dataset['for'] = id;
+        completeBtn.name = 'DoneTask';
+        completeBtn.classList.add('fa');
+        completeBtn.classList.add('fa-check');
+        completeBtn.classList.add('fa-2x');
+        completeBtn.classList.add('text-success');
+        return completeBtn;
+    };
+
+    const createDeleteButton = (id) => 
+    {
+        const deleteBtn = document.createElement('i');
+        deleteBtn.dataset['for'] = id;
+        deleteBtn.name = 'DeleteTask';
+        deleteBtn.classList.add('fa');
+        deleteBtn.classList.add('fa-times');
+        deleteBtn.classList.add('fa-2x');
+        deleteBtn.classList.add('text-danger');
+        return deleteBtn;
+    };
+
+    const findTaskById = (taskId) => todoList.querySelector("li[data-id='" + taskId + "']");
+
+    btnAdd.addEventListener('click', OnAddBtnClicked);
     input.addEventListener('keydown', (e) => e.keyCode === 13 ? OnAddBtnClicked() : () => {})
-    document.getElementById('TodoList').addEventListener('click', OnTaskClicked);
+    todoList.addEventListener('click', OnTaskClicked);
     
 });
